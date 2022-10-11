1ea66a6604effffb5f30d738aa0b6496ca1e4d58
hazlomcodes <harry.lomas1@outlook.com>
2022-10-11 15:23:09 +0100

    solved objects problems

--- src/objects.js
+++ src/objects.js
@@ -7,40 +7,57 @@ const createPerson = (name, age) => {
 };
 
 const getName = object => {
-  return object. name;
-};
+return object.name;
+}
+
 
 const getProperty = (property, object) => {
- return object.getProperty;
-};
+return object[property];
+}
+
 
 const hasProperty = (property, object) => {
+if (object.hasOwnProperty(property)) {
+return true;
+}
+return false;
   
-  }
 };
 
 const isOver65 = person => {
-  // your code here
+  if (person.age >65) {
+    return true;
+  }
+  return false;
 };
 
 const getAges = people => {
-  // your code here
+  return people.map(person => person.age);
 };
 
 const findByName = (name, people) => {
-  // your code here
+  return people.find(object => object.name === name);
 };
 
 const findHondas = cars => {
-  // your code here
+  return cars.filter(object => object.manufacturer === 'Honda');
 };
 
 const averageAge = people => {
-  // your code here
+  return people.reduce((prev, user) => prev + user.age, 0) / people.length;
+
 };
 
 const createTalkingPerson = (name, age) => {
-  // your code here
+  return {
+      name: name,
+      age: age,
+      introduce: function(AI) {
+        return `Hi ${AI}, my name is ${name} and I am ${age}!`
+    
+    } 
+  }
+  
 };
 
 module.exports = {
